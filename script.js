7f3ffd8181015a5e440d6d2eb9d41d6ec0d542bf
nafilasalsavira <nafila095@gmail.com>
2024-11-11 15:42:16 +0700

    e
    e

---
+++ script.js
@@ -0,0 +1,9 @@
+document.querySelectiorAll('a[href^="#"').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
